90938dcd7f6551adc14ad89e83b214c95a896e62
Your Name <youremail@domain.co>
2018-07-16 17:25:53 +0700

    Edit blog

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {
+    return view('Test');
+});
+
+Route::get('/page', function () {
+    return view('staticpage');
+});
